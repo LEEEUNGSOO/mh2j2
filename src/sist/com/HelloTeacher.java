0ad107084090315cbbd2ab3b984aa3d99ee5e0d7
osiris0607 <osiris0607@gmail.com>
2020-11-26 17:14:56 +0900

    배고파

--- src/sist/com/HelloTeacher.java
+++ src/sist/com/HelloTeacher.java
@@ -5,6 +5,7 @@ public class HelloTeacher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("소고기 먹고싶어용");
+		System.out.println("대방어 먹고싶어용");
 	}
 
 }
