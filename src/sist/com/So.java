514e27743321f4a000781ae46baa21f7cc94e516
user <user@DESKTOP-2RQJQBT>
2020-11-26 17:39:10 +0900

    soso

---
+++ src/sist/com/So.java
@@ -0,0 +1,10 @@
+package sist.com;
+
+public class So {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Å×½ºÆ®");
+	}
+
+}
--- src/sist/com/TestJava.java
+++ src/sist/com/TestJava.java
@@ -5,6 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("FirstGitMessage");
 		System.out.println("È¿Àº Â¯");
+		
 	}
 
 }
