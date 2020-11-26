28a0aa48e4a45eb92f520785551c1016b520b307
osiris0607 <osiris0607@gmail.com>
2020-11-26 17:10:38 +0900

    11/26 5:10

--- src/sist/com/TestJava.java
+++ src/sist/com/TestJava.java
@@ -5,6 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("FirstGitMessage");
 		System.out.println("È¿Àº Â¯");
+		System.out.println("Ç®ÇÏ¼¼¿ä È¿ÀºÂ¯");
 	}
 
 }
