29b11bbff7bcaacd9a564f2dd926d08cb3ec9de1
user <user@DESKTOP-2RQJQBT>
2020-11-26 17:41:33 +0900

    뀨

--- src/sist/com/So.java
+++ src/sist/com/So.java
@@ -4,7 +4,8 @@ public class So {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("테스트");
+		System.out.println("테스트1");
+		System.out.println("테스트2");
 	}
 
 }
